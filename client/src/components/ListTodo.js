12989bb5877f146144b2cb8c0f0997fd01b1326f
shakibHossain <shakib.hossain.sh@gmail.com>
2020-08-25 16:54:45 -0600

    show description from db on list component

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 const ListTodo = () => {
-    return <h1>List Todos</h1>;
+    const [todos, setTodos] = useState([]);
+
+    const getTodos = async e => {
+        try {
+            const response = await fetch("http://localhost:5001/todos");
+            const jsonData = await response.json();
+
+            setTodos(jsonData);
+            
+        } catch (error) {
+            console.log(error.message);
+            
+        }
+
+    }
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
+        <Fragment>
+            <table class="table mt-5">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                {/*
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>
+                */}
+                {todos.map(todo => (
+                    <tr>
+                        <td>{todo.description}</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>
+                ))}
+                
+                
+                </tbody>
+            </table>
+        </Fragment>
+    );
 };
 
 export default ListTodo;
